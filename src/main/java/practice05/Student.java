0dbd3b6034ced76af943a9eb5627e2bf599ab1a4
BbillyMai <1363543881@qq.com>
2019-07-11 21:00:36 +0800

    finished practice01 to 06

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,18 @@
 package practice05;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+
+    public Student(String name, int age, int kclass) {
+        super(name, age);
+        this.klass = kclass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        return String.format("%s I am a Student. I am at Class %d.",super.introduce(), klass);
+    }
 }
